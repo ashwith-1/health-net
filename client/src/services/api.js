d83d4cd1df5cf3973fa7eb88bcb2def44f946c87
ashwith-1 <ashwithpanakanti@gmail.com>
2026-05-30 10:16:32 +0530

    reset api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://health-net-8pna.onrender.com/auth",
+  baseURL: "https://health-net-8pna.onrender.com",
 });
 
 // ================= TOKEN ATTACH =================
