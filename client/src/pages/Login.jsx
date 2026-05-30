390be80c1f3fc11d1a37af43fe511f66e906ad8a
ashwith-1 <ashwithpanakanti@gmail.com>
2026-05-30 10:34:52 +0530

    routes fix

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
       setLoading(true);
 
       // 🔐 LOGIN API CALL
-      const res = await API.post("/auth/login", {
+      const res = await API.post("/login", {
         email,
         password,
       });
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -40,7 +40,7 @@ function Register() {
       setLoading(true);
 
       await API.post(
-        "/auth/register",
+        "/register",
         formData
       );
 
